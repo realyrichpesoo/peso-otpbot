bf08de8622bf8dc5396cb9d204ddd908f4bbe7a1
realyrichpesoo <73573094+realyrichpesoo@users.noreply.github.com>
2021-09-03 09:00:50 +0800

    1

---
+++ bots/discord/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiurl: 'http://54.172.96.82:1337',
+    apipassword: 'zBBq3WJwtQXPyRFrw0B9ows3geQilXe5',
+
+    discordtoken: 'ODgxNjkwMDQwNTI5NDc3NjQy.YSwf1g.D68Q1Qcp37Ogh-CElAiq5scj9T0',
+    discordprefix: '!',
+    secretpassword: 'IWAFbg6S43qzf0x1r54s7Zg3SR6fe35',
+    botuser_rolename: 'Bot User',
+    admin_rolename: 'Admin'
+};
